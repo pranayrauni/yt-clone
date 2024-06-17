f258d6a8bc2dd55df5773b307fbe7e56f2b5e639
Pranay Singh <ipranaysingh@gmail.com>
2024-06-17 14:21:41 +0530

    project setup and api setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {AppContext} from "./context/contextApi"
+
+const App = () => {
+  return (
+    <AppContext>
+      <div className='text-4xl'>App</div>
+    </AppContext>
+    
+  )
+}
+
+export default App
